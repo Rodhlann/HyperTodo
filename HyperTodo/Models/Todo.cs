0dd6b98275d4baf7bb25bf9481011e8276d49087
Tim Pepper <tjpepper93@gmail.com>
2018-03-19 23:06:17 -0400

    Set up database. Create tables and matching models.

---
+++ HyperTodo/Models/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HyperTodo.Models
+{
+    [Table(name: "Todos")]
+    public class Todo
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public String Note { get; set; }
+        public enum Priority {
+            NONE,
+            LOW,
+            MEDIUM,
+            HIGH
+        };
+    }
+}
--- HyperTodo/Program.cs
+++ HyperTodo/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using HyperTodo.Models;
+using System.Data.SqlClient;
 
 namespace HyperTodo
 {
